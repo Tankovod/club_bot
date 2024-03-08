869a916ab48c3103348fc750a49926560b6872d5
dmitry <Wotdimas@gmail.com>
2024-03-08 19:37:20 +0300

    push me

--- src/database/models.py
+++ src/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, BIGINT, VARCHAR, TIMESTAMP, ForeignKey, SMALLINT, TEXT, BOOLEAN
+from sqlalchemy import Column, BIGINT, VARCHAR, TIMESTAMP, ForeignKey, SMALLINT, TEXT, BOOLEAN, INT
 from sqlalchemy.orm import relationship
 
 from .base import Base
@@ -11,13 +11,13 @@ class User(Base):
     first_name = Column(VARCHAR(128), nullable=True, unique=False)
     last_name = Column(VARCHAR(128), nullable=True, unique=False)
     phone = Column(VARCHAR(16), nullable=True, unique=True)
-    role_id = Column(ForeignKey(column="role.id", ondelete="RESTRICT"), nullable=False, unique=False)
+    role_id = Column(SMALLINT, ForeignKey(column="role.id", ondelete="RESTRICT"), nullable=False, unique=False)
     date_sign_up = Column(TIMESTAMP, nullable=False, unique=False)
     is_male = Column(BOOLEAN, nullable=True, unique=False, default=None)
     note = Column(VARCHAR(128), nullable=True, unique=False)
 
     user_role = relationship(argument="Role", back_populates="users", lazy="selectin")
-    # clubs = relationship(argument="UserClub", back_populates="user", lazy="selectin")
+    clubs = relationship(argument="UserClub", back_populates="user", lazy="selectin")
 
     def __str__(self):
         if self.first_name and self.last_name:
@@ -54,10 +54,10 @@ class Club(Base):
     photo = Column(VARCHAR(512), nullable=True, unique=False)
     description = Column(TEXT, nullable=False, unique=False)
 
-    # users = relationship(argument="UserClub", back_populates="club", lazy="selectin")
+    users = relationship(argument="UserClub", back_populates="club", lazy="selectin")
 
     def __str__(self):
-        return self.name
+        return self.tag
 
     def __repr__(self):
         return str(self)
@@ -66,11 +66,11 @@ class Club(Base):
 class UserClub(Base):
     __tablename__ = "user_club"
     id = Column(BIGINT, primary_key=True)
-    user_id = Column(ForeignKey("users.tg_id", ondelete="CASCADE"), nullable=False, unique=False)
-    club_id = Column(ForeignKey("club.id", ondelete="CASCADE"), nullable=False, unique=False)
+    user_id = Column(BIGINT, ForeignKey(column="users.tg_id", ondelete="CASCADE"), nullable=False, unique=False)
+    club_id = Column(INT, ForeignKey(column="club.id", ondelete="CASCADE"), nullable=False, unique=False)
 
-    user = relationship(argument="User")  # , back_populates="clubs", lazy="selectin")
-    club = relationship(argument="Club")
+    user = relationship(argument="User", back_populates="clubs", lazy="selectin")
+    club = relationship(argument="Club", back_populates="users", lazy="selectin")
 
     def __str__(self):
         return self.user + " " + self.club
